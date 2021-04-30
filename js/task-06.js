41d8f4c3002cd6a02bbbd195609daf4d47baf17c
kokhanskyi <79640742+kokhanskyi@users.noreply.github.com>
2021-04-30 16:00:50 +0300

    dz-7!

--- js/task-06.js
+++ js/task-06.js
@@ -3,19 +3,15 @@
 const inputText = document.querySelector('#validation-input');
 const inputNum = inputText.getAttribute('data-length');
 
-// inputText.addEventListener('blur', event => {
-//   if (inputText.value.length === +inputNum) {
-//     inputText.classList.add('valid');
-//   } else {
-//     inputText.classList.add('invalid');
-//   }
-// });
-
 inputText.addEventListener('blur', event => {
-  inputText.value.length === +inputNum
-    ? inputText.classList.add('valid')
-    : inputText.classList.add('invalid');
+  if (inputText.value.length === +inputNum) {
+    inputText.classList.add('valid');
+  } else {
+    inputText.classList.add('invalid');
+  }
 });
+
 inputText.addEventListener('focus', event => {
+  inputText.classList.remove('valid');
   inputText.classList.remove('invalid');
 });
